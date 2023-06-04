0551c52d28892c4c4c88240dc725624ad7c03499
Adam Eury <euryadam@gmail.com>
2023-06-03 22:23:44 -0400

    Update temperature.go

--- temperature.go
+++ temperature.go
@@ -3,7 +3,7 @@ package weather
 // Temperature is a type that stores its value with the unit Kelvin.
 type Temperature float64
 
-// Celsius returns value of the temperature in Celsius.
+// Celsius returns the value of the temperature in Celsius.
 func (t Temperature) Celsius() float64 {
 	return float64(t) - 273.15
 }
